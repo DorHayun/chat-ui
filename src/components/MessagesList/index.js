b59d0e3b6c8a9f20014f43961a0974980d8787b3
Dor Hayun <dorhayun7@gmail.com>
2018-11-03 23:18:12 +0200

    Add Appbar and use new logo

--- src/components/MessagesList/index.js
+++ src/components/MessagesList/index.js
@@ -1,22 +1,38 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import MessageBubble from '../MessageBubble'
 import './MessagesList.css'
 
-
-
-
 class MessagesList extends React.Component {
+  state = {
+    scrollableList: null
+  }
+
+  componentDidMount() {
+    const scrollableList = ReactDOM.findDOMNode(this.refs.scrollableList)
+    this.setState({scrollableList})
+  }
+
+  scrollToPosition = () => {
+    if (this.state.scrollableList) {
+      this.state.scrollableList.scrollTo(0, this.state.scrollableList.scrollHeight)
+    }
+  }
+  
   render() {
-    const { messages, name } = this.props
+    const { messages, userUuid } = this.props
+    this.scrollToPosition()
     return (
-      <div className="messages-list">
-        {
-          messages.map((message, index) => (
-            <MessageBubble key={index} name={message.name} text={message.text} isMine={name === message.name} />)
-          )
-        }
+      <div className="messages-list" ref="scrollableList">
+        {messages.map((message, index) => (
+          <MessageBubble
+            key={index}
+            name={message.name}
+            text={message.text}
+            isMine={userUuid === message.userUuid}
+          />
+        ))}
       </div>
-     
     )
   }
 }
